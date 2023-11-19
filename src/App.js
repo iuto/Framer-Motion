4de5b55e68b1e7104200cbbb04b0838d4e8f734f
iuto <baiyinyiteng555@gmail.com>
2023-11-19 19:47:52 +0900

    :wastebasket:不要なファイルを削除

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // src/App.js
 import React from 'react';
 import ProfileCard from './components/ProfileCard'; 
-import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <ProfileCard /> {/* ProfileCard コンポーネントをレンダリング */}
+      <ProfileCard />
     </div>
   );
 }
